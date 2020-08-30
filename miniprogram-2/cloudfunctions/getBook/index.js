99bebf134815b7211141153ab640a6b814a36a14
dingqiqi <dingqiqi8@qq.com>
2020-08-30 20:31:21 +0800

    添加图书功能完成

---
+++ miniprogram-2/cloudfunctions/getBook/index.js
@@ -0,0 +1,53 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+const axios = require('axios')
+const doubanbook = require('doubanbook')
+const cheerio = require('cheerio')
+
+cloud.init()
+
+async function searchBook(isbn) {
+  console.log("-->", isbn)
+  let url = "https://search.douban.com/book/subject_search?search_text=" + isbn
+  let searchInfo = await axios.get(url)
+  let reg = /window\.__DATA__ = "(.*)"/
+  if (reg.test(searchInfo.data)) {
+    let bookData = doubanbook(RegExp.$1)[0]
+    console.log("-->", bookData)
+    console.log(bookData.title, bookData.rating, bookData.url)
+    return await getDouBanBookInfo(bookData)
+  }
+  return null
+}
+
+async function getDouBanBookInfo(bookData) {
+  let doubanbookInfo = await axios.get(bookData.url)
+  // console.log("-->", doubanbookInfo.data)
+  const $ = cheerio.load(doubanbookInfo.data)
+  // 去除换行  空格  过滤空值
+  let info = $("#info").text().split('\n').map(v => v.trim()).filter(v => v)
+  // console.log("-->", info)
+  const auther = info[1]
+  const tags = []
+  $("#db-tags-section a.tag").each((k, v) => {
+    tags.push($(v).text())
+  })
+  const res = {
+    createTime: new Date().getTime(),
+    title: bookData.title,
+    rating: bookData.rating["star_count"] / bookData.rating["value"],
+    image: bookData.cover_url,
+    tags: tags,
+    auther: auther
+  }
+  console.log("-->", res)
+  return res
+}
+
+// console.log(searchBook(9787040432022))
+// 云函数入口函数
+exports.main = async (event, context) => {
+  // 解包  客户端要key:value形式调用
+  const { isbn } = event
+  return searchBook(isbn)
+}
--- miniprogram-2/miniprogram/pages/me/me.js
+++ miniprogram-2/miniprogram/pages/me/me.js
@@ -1,22 +1,76 @@
 // pages/me/me.js
+const db = wx.cloud.database()
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    userInfo:{}
+    userInfo: wx.getStorageSync('userInfo') || {}
   },
-  onUserInfo(event){
+  addBook(bookInfo) {
+    db.collection("douban_books").add({
+      data: bookInfo
+    }).then(res => {
+      console.log(res)
+      if (res._id) {
+        wx.showModal({
+          title: '系统提示',
+          content: `<${bookInfo.title}>` + "添加成功",
+          showCancel: false
+        })
+      }
+    })
+  },
+  searchBook(isbn) {
+    wx.cloud.callFunction({
+      name: "getBook",
+      data: {
+        isbn: isbn
+      },
+      complete: (res) => {
+        console.log(res.result)
+        if (res.result.title) {
+          this.addBook(res.result)
+        } else {
+          wx.showToast({
+            title: '"图书查找失败"',
+            icon: 'warn',
+            duration: 1000
+          })
+        }
+      }
+    })
+  },
+  scanBook() {
+    // this.addBook(9787040432022)
+    wx.scanCode({
+      onlyFromCamera: false,
+      success: (res) => {
+        console.log(res.result)
+        this.searchBook(res.result)
+      },
+      fail: (res) => {
+        wx.showToast({
+          title: '"扫码失败"',
+          icon: 'warn',
+          duration: 1000
+        })
+      }
+    })
+  },
+  onUserInfo(event) {
     console.log(event)
     this.data.userInfo = event.detail.userInfo
     wx.cloud.callFunction({
-      name:"login",
-      complete:res=>{
+      name: "login",
+      complete: res => {
         console.log(res.result)
         this.data.userInfo.openid = res.result.openid
         console.log(this.data.userInfo)
         this.setData(this.data)
+        wx.setStorageSync('userInfo', this.data.userInfo)
       }
     })
   },
